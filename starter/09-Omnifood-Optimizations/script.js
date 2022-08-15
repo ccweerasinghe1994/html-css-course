b31c92a9d9aa3300cd29ac60c1aefe369ddcd70d
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2022-08-15 08:13:15 +0530

    📜🆕: adding the script file

--- starter/09-Omnifood-Optimizations/script.js
+++ starter/09-Omnifood-Optimizations/script.js
@@ -6,25 +6,43 @@ btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
 
-///////////////////////////////////////////////////////////
-// Fixing flexbox gap property missing in some Safari versions
-function checkFlexGap() {
-  var flex = document.createElement("div");
-  flex.style.display = "flex";
-  flex.style.flexDirection = "column";
-  flex.style.rowGap = "1px";
+// Smooth Scrolling animation
 
-  flex.appendChild(document.createElement("div"));
-  flex.appendChild(document.createElement("div"));
+const allLinks = document.querySelectorAll("a:link");
 
-  document.body.appendChild(flex);
-  var isSupported = flex.scrollHeight === 1;
-  flex.parentNode.removeChild(flex);
-  console.log(isSupported);
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const target = this.getAttribute("href");
 
-  if (!isSupported) document.body.classList.add("no-flexbox-gap");
-}
-checkFlexGap();
+    if (target === "#") {
+      window.scroll({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+
+  }),
+
+    ///////////////////////////////////////////////////////////
+    // Fixing flexbox gap property missing in some Safari versions
+    function checkFlexGap() {
+      var flex = document.createElement("div");
+      flex.style.display = "flex";
+      flex.style.flexDirection = "column";
+      flex.style.rowGap = "1px";
+
+      flex.appendChild(document.createElement("div"));
+      flex.appendChild(document.createElement("div"));
+
+      document.body.appendChild(flex);
+      var isSupported = flex.scrollHeight === 1;
+      flex.parentNode.removeChild(flex);
+      console.log(isSupported);
+
+      if (!isSupported) document.body.classList.add("no-flexbox-gap");
+    }
+  checkFlexGap();
 
 // https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
 
